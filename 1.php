5a526746c46b112a9c0325103730ca2b2c66e34f
VladLihovoy <lihovoy.v@gmail.com>
2016-06-14 13:20:58 +0000

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,16 @@
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Влад
+ * Date: 13.06.2016
+ * Time: 21:05
+ */
+$x = 5;
+$y = 8;
+$sum = $x + $y;
+$proiz = $x * $y;
+echo $sum."<br />";
+echo $proiz;
+?>
